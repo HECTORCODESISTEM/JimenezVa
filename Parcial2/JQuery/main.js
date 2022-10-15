d34c4d382b70bd894294127aa173528f13fb5d0f
Hector Jimenez <vector_king@outlook.es>
2022-10-14 22:34:41 -0500

    update

--- Parcial2/JQuery/main.js
+++ Parcial2/JQuery/main.js
@@ -1,1 +1,39 @@
-$("selector")
+$(function(){
+
+    $("#primero").css({"background-color" : "red"});
+
+    $("#boton4").click(function()
+    { $(".texto").fadeOut(); 
+    alert("No nos queremos ir señor programador");
+    alert("Esto que has echo no tiene remedio :("); }
+    
+    );
+
+
+
+
+// Botones Atributos
+    $("#boton1").click(function()
+    { alert("Hola");}
+    );
+
+    $("#boton2").click(function()
+    { $("#segundo").hide();}
+    );
+
+    $("#boton3").click(function()
+    { $("#segundo").show();}
+    );
+// Flex box atributos
+    $(".contenedor").mouseenter(function()
+    { $("#broma").hide(); }
+    
+    );
+
+    $(".contenedor").mouseleave(function()
+    { $("#broma").show();}
+    );
+
+
+
+})
